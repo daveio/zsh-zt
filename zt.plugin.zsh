941206b7f93ad9059af5b0248072414d9537f3fc
Dave Williams <dave@dave.io>
2019-04-07 00:04:37 +0100

    finish renaming properly

--- zt.plugin.zsh
+++ zt.plugin.zsh
@@ -2,6 +2,6 @@
 # https://github.com/daveio/zsh-zt
 # vim:ai:ff=unix:fenc=utf-8:ts=2:et:nu:wrap
 
-_gotp_ci_test () {
+_zt_ci_test () {
   echo "inside ci test function with ${#} args, first arg ${1}"
 }
